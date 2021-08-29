370be108d9c4675e8b9a4b88aa50f96fcf53a276
gagandhillon045 <gagan.dhillon045@gmail.com>
2021-08-29 00:52:58 -0400

    add user

---
+++ scripts/create-user.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo airflow users create \
+    --username somename \
+    --firstname somename \
+    --lastname somename \
+    --role Admin \
+    --password somename
+    --email somename@superhero.org
