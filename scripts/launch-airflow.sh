33e35bd5885ffc9867033f98aa21d2cc4a91f891
gagandhillon045 <gagan.dhillon045@gmail.com>
2021-08-29 00:23:07 -0400

    modified some of the files

--- scripts/launch-airflow.sh
+++ scripts/launch-airflow.sh
@@ -1,19 +1,20 @@
 # initialize the database
-airflow db init
+sudo airflow db init
 
 sudo airflow users create \
     --username admin \
     --firstname Peter \
     --lastname Parker \
     --role Admin \
+    --password admin
     --email spiderman@superhero.org
 
 # start the web server, default port is 8080
-airflow webserver --port 8080
+sudo airflow webserver --port 80
 
 # start the scheduler
 # open a new terminal or else run webserver with ``-D`` option to run it as a daemon
-airflow scheduler
+sudo airflow scheduler
 
 # visit localhost:8080 in the browser and use the admin account you just
 # created to login. Enable the example_bash_operator dag in the home page
