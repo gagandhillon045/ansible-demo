14e34bb5a5c68c3386bc1a5032194c7f5cfd07bc
Ubuntu <ubuntu@ip-172-31-46-236.us-east-2.compute.internal>
2021-10-07 18:49:14 +0000

    inserted

--- scripts/launch-airflow.sh
+++ scripts/launch-airflow.sh
@@ -1,3 +1,14 @@
+
+#!/bin/sh
+
+AIRFLOW_VERSION=2.1.3
+PYTHON_VERSION="$(python3 --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+
+# For example: 3.6
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+# For example: https://raw.githubusercontent.com/apache/airflow/constraints-2.1.3/constraints-3.6.txt
+pip3 install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+
 # initialize the database
 sudo airflow db init
 
